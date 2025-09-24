a12c85165360df4f19fbd35be3409acc56f40ea2
Michael-Flynn <109002599+Michael-Flynn@users.noreply.github.com>
2025-09-23 20:50:20 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import logo from './img/raiderlotto.png';
+import rick from '.img/rick.gif';
 import RaffleDrawing from './RaffleDrawing';
 
 function App() {
   return (
     <div className="App">
       <img alt="" src={logo} width={200} height={200} />
+      <img alt="" src={rick} width={200} height={200} />
       <RaffleDrawing />
     </div>
   );
